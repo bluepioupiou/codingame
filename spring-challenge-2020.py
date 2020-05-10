c7732fa58c6e362049cf369dce90195acc7d81eb
Alain begey <bluepioupiou@gmail.com>
2020-05-10 02:31:19 +0200

    gone from place 527 to place 450 in bronze

--- spring-challenge-2020.py
+++ spring-challenge-2020.py
@@ -1,54 +1,16 @@
 import sys
 import math
 import copy
+from typing import List
 
 NEIGHBOURS_DIRECTIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-MAX_PATH_LENGTH = 10
+MAX_PATH_LENGTH = 5
 
-
-class Path:
-    def __init__(self, tile):
-        self.tiles = [tile]
-        self.value = 0
-
-    def __str__(self):
-        formatedList = ["({0},{1})".format(str(tile.x), str(tile.y))
-                        for tile in self.tiles]
-        return "{0} {1}".format(",".join(formatedList), self.value)
-
-    def addTile(self, tile):
-        self.tiles.append(tile)
-        self.value += int(tile.value)
-
-    def getLastTile(self):
-        return self.tiles[-1]
-
-
-class PacMan:
-    TYPE_ROCK = "ROCK"
-    TYPE_PAPER = "TYPE_PAPER"
-    TYPE_SCISSORS = "SCISSORS"
-
-    def __init__(self, id, x, y, mine, type_id, speed_turns_left, ability_cooldown):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.mine = mine
-        self.type_id = type_id
-        self.speed_turns_left = speed_turns_left
-        self.ability_cooldown = ability_cooldown
-
-    def __str__(self):
-        return "{0}:{1}".format(self.x, self.y)
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def isOnTile(self, tile):
-        return self.x == tile.x and self.y == tile.y
-
-    def canEat(self, other):
-        return (self.type_id == self.TYPE_ROCK and other.type_id == self.TYPE_SCISSORS) or (self.type_id == self.TYPE_PAPER and other.type_id == self.TYPE_ROCK) or (self.type_id == self.TYPE_SCISSORS and other.type_id == self.TYPE_PAPER)
+DISTANCE_WEIGHT = 0.91
+FRIEND_PACMAN_VALUE = 10
+SAFE_ENEMY_VALUE = 20
+DANGEROUS_ENEMY_VALUE = 20
+ENEMY_VALUE = 10
 
 
 class Tile:
@@ -64,6 +26,96 @@ class Tile:
         return self.x == other.x and self.y == other.y
 
 
+class Pacman:
+    TYPE_ROCK = "ROCK"
+    TYPE_PAPER = "PAPER"
+    TYPE_SCISSORS = "SCISSORS"
+
+    def __init__(self, id, x, y, mine, type_id, speed_turns_left, ability_cooldown):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.mine = mine
+        self.type_id = type_id
+        self.speed_turns_left = speed_turns_left
+        self.ability_cooldown = ability_cooldown
+        self.speeding = False
+
+    def __str__(self):
+        return "{0}:{1}".format(self.x, self.y)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def isOnTile(self, tile):
+        return self.x == tile.x and self.y == tile.y
+
+    def canEat(self, other):
+        return (self.type_id == self.TYPE_ROCK and other.type_id == self.TYPE_SCISSORS) or (self.type_id == self.TYPE_PAPER and other.type_id == self.TYPE_ROCK) or (self.type_id == self.TYPE_SCISSORS and other.type_id == self.TYPE_PAPER)
+
+
+class Path:
+    def __init__(self, tile):
+        self.tiles = [tile]
+        self.value = float(0)
+        self.encounters = []
+
+    def addTile(self, tile: Tile):
+        self.value += float(tile.value * DISTANCE_WEIGHT ** len(self.tiles))
+
+        if(tile.value == 10):
+            self.encounters.append("X")
+        self.tiles.append(tile)
+
+    def calculateWeight(self, board, myPacman: Pacman, pacmen: List[Pacman]):
+        # TODO: Rajouter de voir si son pouvoir sera dispo à temps quand je l'atteindrai
+        for tile in self.tiles[1:]:
+            for pacman in pacmen:
+                neightbours = board.getNeighbours(tile)
+                for i, inspectTile in enumerate([tile] + neightbours):
+                    # Ca ne sert a rien de regarder si le pacman est sur une case du path pour les voisins
+                    if i == 0 or not inspectTile in self.tiles:
+                        if pacman.isOnTile(inspectTile):
+                            if pacman.mine:  # Un des miens
+                                self.value -= float(FRIEND_PACMAN_VALUE * (DISTANCE_WEIGHT ** len(
+                                    self.tiles))) / 1 if i == 0 else 2
+                                self.encounters.append("F")
+                            else:  # un ennemi
+                                if myPacman.canEat(pacman):  # je peux le manger
+                                    self.value += float(SAFE_ENEMY_VALUE * (DISTANCE_WEIGHT ** len(
+                                        self.tiles))) / 1 if i == 0 else 2
+                                    self.encounters.append("S")
+                                elif pacman.canEat(myPacman):  # il peut me manger
+                                    self.value -= float(DANGEROUS_ENEMY_VALUE * (DISTANCE_WEIGHT ** len(
+                                        self.tiles))) / 1 if i == 0 else 2
+                                    self.encounters.append("D")
+                                else:  # Il est comme moi
+                                    self.value -= float(ENEMY_VALUE * (DISTANCE_WEIGHT **
+                                                                       len(self.tiles))) / 1 if i == 0 else 2
+                                    self.encounters.append("E")
+
+    def getLastTile(self):
+        return self.tiles[-1]
+
+    def getDirections(self):
+        directions = []
+        for i, tile in enumerate(self.tiles[1:]):
+            difference = [tile.x - self.tiles[i].x, tile.y - self.tiles[i].y]
+            if difference == [-1, 0]:
+                directions.append("◄")
+            elif difference == [1, 0]:
+                directions.append("►")
+            elif difference == [0, -1]:
+                directions.append("▲")
+            elif difference == [0, 1]:
+                directions.append("▼")
+        return "".join(directions)
+
+    def print(self):
+        print(" - {0:.2f}:{1} ({2})".format(self.value, self.getDirections(),
+                                            "".join(self.encounters)), file=sys.stderr)
+
+
 class Board:
     def __init__(self, width, height):
         self.width = width
@@ -74,6 +126,12 @@ class Board:
         for row in self.rows:
             print("".join(["X" if car == 10 else "-" if car == 0 else "+" if car == 1 else " "
                            for car in row]), file=sys.stderr)
+
+    def resetXPellets(self):
+        for y, row in enumerate(self.rows):
+            for x, tileValue in enumerate(row):
+                if tileValue == 10:
+                    self.setTile(x, y, 0)
 
     def setTile(self, x, y, value):
         self.rows[y][x] = value
@@ -110,7 +168,7 @@ class Board:
                     best_tile = self.getTile(x, y)
         return best_tile
 
-    def findBestPaths(self, pacman):
+    def findPossiblePaths(self, pacman):
         path = Path(self.getTile(pacman.x, pacman.y))
         paths = [path]
         self.calculatePossiblePaths(paths, path)
@@ -134,104 +192,73 @@ class Board:
 class Game:
     def __init__(self, width, height):
         self.myPacMen = []
-        self.powerPacMen = []
         self.enemyPacMen = []
         self.commands = []
         self.board = Board(width, height)
 
     def reset(self):
         self.myPacMen = []
-        self.powerPacMen = []
         self.enemyPacMen = []
         self.commands = []
+        self.board.resetXPellets()
 
     def movePacMen(self):
-        for pacMan in self.myPacMen:
-            if pacMan not in self.powerPacMen:
-                self.addBestMove(pacMan)
+        for pacman in self.myPacMen:
+            if not pacman.speeding:
+                self.addBestMove(pacman)
 
     def resolvePowers(self):
-        for pacMan in self.myPacMen:
-            if not pacMan.ability_cooldown:
-                self.speedCommand(pacMan)
-                self.powerPacMen.append(pacMan)
-
-    def haveToAvoidTile(self, tile, myPacMan):
-        # TODO : ne faire attention que si PacMan dangereux et virer les miens si on traite les changements de board
-        for pacMan in self.enemyPacMen + self.myPacMen:
-            if myPacMan == pacMan:
-                return False
-            if pacMan.isOnTile(tile):
-                if pacMan.mine:
-                    return True
-
-                if myPacMan.canEat(pacMan):
-                    print("{0} can eat {1}".format(
-                        myPacMan.id, pacMan.id), file=sys.stderr)
-                    return False
-                return True
-
-            if pacMan.mine:
-                return False
-
-            neightbours = self.board.getNeighbours(tile)
-            for neightbour in neightbours:
-                if pacMan.isOnTile(neightbour):
-                    if myPacMan.canEat(pacMan):
-                        print("{0} could eat {1}".format(
-                            myPacMan.id, pacMan.id), file=sys.stderr)
-                        return False
-
-                    if pacMan.canEat(myPacMan):
-                        print("{0} could be eaten by {1}".format(
-                            myPacMan.id, pacMan.id), file=sys.stderr)
-                        return True
-
-                    return False
-        return False
+        for pacman in self.myPacMen:
+            if not pacman.ability_cooldown:
+                self.speedCommand(pacman)
 
     def addBestMove(self, pacman):
-        paths = self.board.findBestPaths(pacman)
-        # Vire les chemins ou on rencontre un autre pacman
-        paths = list(
-            filter(lambda path: len(path.tiles) == len(list(filter(lambda tile: not self.haveToAvoidTile(tile, pacman), path.tiles))), paths))
+        paths = self.board.findPossiblePaths(pacman)
 
+        # Ajoute les poids pour chaque chemin
+        for path in paths:
+            path.calculateWeight(self.board, pacman,
+                                 self.myPacMen + self.enemyPacMen)
+
+        # Trie les chemins par valeur décroissante
         paths.sort(key=lambda x: x.value, reverse=True)
-        sameValuePaths = list(
-            filter(lambda path: path.value == paths[0].value, paths))
-        # print("{0} chemins avec la valeur {1}".format(
-        #    len(sameValuePaths), paths[0].value), file=sys.stderr)
-        sameValuePaths.sort(key=lambda x: len(x.tiles))
-        beginWithPelletPaths = list(
-            filter(lambda path: not path.tiles[1].value == 0, paths))
-        chosenPath = None
-        if len(beginWithPelletPaths):
-            chosenPath = beginWithPelletPaths[0]
-            message = "Meilleur chemin, valeur {0}, destination {1}".format(
-                chosenPath.value, chosenPath.tiles[-1])
-        elif len(sameValuePaths):
-            chosenPath = sameValuePaths[0]
-            message = "Alternative, valeur {0}, destination {1}".format(
-                chosenPath.value, chosenPath.tiles[-1])
+        # Ne garde que les chemins avec valeur > 0
+        paths = list(filter(lambda path: path.value > 0, paths))
 
-        # Si le chemin ne rencontre aucune pellet, trouve la plus proche
-        if not chosenPath or not chosenPath.value:
-            nextTile = self.board.findClosestPellet(pacman)
-            message = "Je trouve rien, je vais au plus proche"
-        else:
+        if len(paths) > 0:
+            chosenPath = paths[0]
             if pacman.speed_turns_left and len(chosenPath.tiles) > 2:
+                self.board.setTile(
+                    chosenPath.tiles[1].x, chosenPath.tiles[1].y, 0)
                 nextTile = chosenPath.tiles[2]
             else:
                 nextTile = chosenPath.tiles[1]
+            if "X" in chosenPath.encounters:
+                message = "MAAAM"
+            elif "S" in chosenPath.encounters:
+                message = "Gotcha!"
+            elif "D" in chosenPath.encounters:
+                message = "Aaahhhh"
+            elif "F" in chosenPath.encounters:
+                message = "Hello Friend"
+            elif "E" in chosenPath.encounters:
+                message = "Ho, Hi"
+            else:
+                message = "Nom!"
+        else:
+            nextTile = self.board.findClosestPellet(pacman)
+            message = "Boooring"
 
+        # TODO : remove when good handle of speed generically
         self.board.setTile(nextTile.x, nextTile.y, 0)
         self.moveCommand(pacman, nextTile, message)
 
     def moveCommand(self, pacman, tile, message):
-        self.addCommand(("MOVE {0} {1} {2} {3}".format(
-            pacman.id, tile.x, tile.y, message)))
+        self.addCommand("MOVE {0} {1} {2} {3}".format(
+            pacman.id, tile.x, tile.y, message))
 
     def speedCommand(self, pacman):
+        pacman.speeding = True
         self.addCommand(("SPEED {0}".format(pacman.id)))
 
     def addCommand(self, command):
@@ -254,9 +281,9 @@ for i in range(height):
 
 # game loop
 while True:
-    game.reset()
     my_score, opponent_score = [int(i) for i in input().split()]
     visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
+    game.reset()
     for i in range(visible_pac_count):
         pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown = input().split()
         pac_id = int(pac_id)
@@ -266,7 +293,7 @@ while True:
         speed_turns_left = int(speed_turns_left)
         ability_cooldown = int(ability_cooldown)
 
-        pacman = PacMan(pac_id, x, y, mine, type_id,
+        pacman = Pacman(pac_id, x, y, mine, type_id,
                         speed_turns_left, ability_cooldown)
         # TODO améliorer la MAJ de la board, ça marche pas
         game.board.setTile(x, y, 0)
@@ -274,15 +301,11 @@ while True:
             game.myPacMen.append(pacman)
         else:
             game.enemyPacMen.append(pacman)
-
     visible_pellet_count = int(input())  # all pellets in sight
     for i in range(visible_pellet_count):
-
         x, y, value = [int(j) for j in input().split()]
-        game.board.setTile(x, y, value)
+        #game.board.setTile(x, y, value)
     # game.board.print()
-
     game.resolvePowers()
     game.movePacMen()
-
     game.printCommands()
